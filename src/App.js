bb1729cb208fadce9ea265670e967dfe1c772815
Chrissy Donovan <christine.n.donovan@gmail.com>
2022-06-06 14:35:29 -0700

    Basic data fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+const WIKIPEDIA_BASE_URL =
+  'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access';
+
+function buildUrl({ year, month, day }) {
+  return `${WIKIPEDIA_BASE_URL}/${year}/${month}/${day}`;
+}
+
 function App() {
+  const [year, setYear] = useState(2015);
+  const [month, setMonth] = useState(10);
+  const [day, setDay] = useState(10);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        const response = await fetch(buildUrl({ year, month, day }));
+        const data = await response.json();
+        setArticles(data.items[0].articles);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchArticles();
+  }, []);
+
+  console.log({ articles });
+
   return <></>;
 }
 
