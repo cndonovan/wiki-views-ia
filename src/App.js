ec46dc3de2f707d8dcf2b5cac030a88b473501b7
Chrissy Donovan <christine.n.donovan@gmail.com>
2022-06-06 15:25:37 -0700

    Wire up the form data to local data and fetching

--- src/App.js
+++ src/App.js
@@ -4,23 +4,34 @@ import './App.css';
 const WIKIPEDIA_BASE_URL =
   'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access';
 
-function buildUrl({ year, month, day }) {
+function buildUrl({ date }) {
+  const year = date.getFullYear();
+  const month = date.getMonth().toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+
   return `${WIKIPEDIA_BASE_URL}/${year}/${month}/${day}`;
 }
 
 const NUM_RESULT_OPTIONS = [25, 50, 75, 100, 200];
 
+function getYesterday() {
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+  return yesterday;
+}
+
 function App() {
-  const [year, setYear] = useState(2015);
-  const [month, setMonth] = useState(10);
-  const [day, setDay] = useState(10);
+  const [date, setDate] = useState(getYesterday());
   const [numResults, setNumResults] = useState(100);
   const [articles, setArticles] = useState([]);
+
+  console.log({ date, numResults, articles });
 
   useEffect(() => {
     async function fetchArticles() {
       try {
-        const response = await fetch(buildUrl({ year, month, day }));
+        const response = await fetch(buildUrl({ date }));
         const data = await response.json();
         setArticles(data.items[0].articles);
       } catch (err) {
@@ -29,22 +40,33 @@ function App() {
     }
 
     fetchArticles();
-  }, []);
-
-  console.log({ articles });
+  }, [date]);
 
   return (
     <div className='app'>
       <form className='form'>
         <div>
-          <label for='date'>Date:</label>
-          <input type='date' id='date' placeholder={new Date()} />
+          <label htmlFor='date'>Date:</label>
+          <input
+            type='date'
+            id='date'
+            value={date.toISOString()}
+            onChange={(e) => {
+              setDate(new Date(e.target.value));
+            }}
+          />
         </div>
         <div>
-          <label for='numResults'>Number of results:</label>
-          <select id='numResults'>
+          <label htmlFor='numResults'>Number of results:</label>
+          <select
+            id='numResults'
+            value={numResults}
+            onChange={(e) => {
+              setNumResults(parseInt(e.target.value, 10));
+            }}
+          >
             {NUM_RESULT_OPTIONS.map((num) => (
-              <option value={num} selected={num === numResults}>
+              <option key={num} value={num} defaultValue={num === numResults}>
                 {num}
               </option>
             ))}
@@ -52,8 +74,8 @@ function App() {
         </div>
       </form>
       <ul>
-        {articles.map((a) => (
-          <li className='article-list-item'>
+        {articles.slice(0, numResults).map((a) => (
+          <li key={a.article} className='article-list-item'>
             <h2>{a.article}</h2>
             <p>Views: {a.views}</p>
           </li>
